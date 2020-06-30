838235aac9780e2feb0ee2688b7e771b859cada4
carflor <cfdeclet@gmail.com>
2020-06-30 14:52:05 -0600

    Add Nav component and basic styling for search and login

---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Components/Nav.css'
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <h1 className="nav-title">DOPE NOPE</h1>
+      {/* ICON IMAGE */}
+      <input className='search-bar'
+      type='search' placeholder='Search Movies...'></input>
+      {/* input has click handler for search */}
+      <button className="login-btn">LOG IN</button>
+      {/* button has to take the click handler */}
+    </nav>
+  )
+}
+
+export default Nav
