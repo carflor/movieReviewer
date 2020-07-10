4ea1ec4896208418aa9b86ed0783b5545c541c20
Bill Wilke <billwilke2@gmail.com>
2020-07-09 22:04:56 -0600

    Fixes test suites now that we changed to router

--- src/App.test.js
+++ src/App.test.js
@@ -3,24 +3,34 @@ import ReactDOM from 'react-dom';
 import { render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 // import { getMovies } from '../apiCalls'
 // jest.mock('../apiCalls.js')
 
 describe('App', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+      <BrowserRouter>
+          <App />
+      </BrowserRouter> , div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('renders loading message', () => {
-  const { getByText } = render(<App />);
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>);
   const linkElement = getByText(/Loading.../);
   expect(linkElement).toBeInTheDocument();
   });
 
   it('Should be able to render the nav items', async () => {
-    const { getByText, getByRole } = render(<App />) 
+    const { getByText, getByRole } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>) 
     const title = await waitFor(() => getByText('DOPE NOPE'))
     const logInButton = await waitFor(() => getByRole('button', {name: 'LOG IN'}))
     expect(title).toBeInTheDocument()
@@ -33,7 +43,10 @@ describe('App', () => {
           json: () => Promise.reject('API is down'),
     })
   );
-    const { getByText } = render(<App />);
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>);
     const linkElement = await waitFor(() => getByText(/Pardon the disturbance in the force.../));
     expect(linkElement).toBeInTheDocument();
   })
--- src/Components/LogInForm.test.js
+++ src/Components/LogInForm.test.js
@@ -2,20 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import App from '../App'
 import LogInForm from './LogInForm'
-import { submitUserLogIn } from '../apiCalls'
+import { submitUserLogIn, getUserMovieRatings } from '../apiCalls'
+import { BrowserRouter } from 'react-router-dom'
 jest.mock('../apiCalls.js')
 
 describe('LogInForm', () => {
   it('should render without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<LogInForm />, div);
+    ReactDOM.render(
+      <BrowserRouter>
+        <LogInForm />
+      </BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
   })
 
   it('should render a log in form', () => {
-    const { getByText, getByPlaceholderText } = render(<LogInForm />)
-    const title = getByText('Log In:')
+    const { getByText, getByPlaceholderText } = render(
+      <BrowserRouter>
+        <LogInForm />
+      </BrowserRouter>)
+    const title = getByText('LOG IN')
     const emailInput = getByPlaceholderText('email')
     const passwordInput = getByPlaceholderText('password')
     const submitBtn = screen.getByRole('button', { name: /Log In/})
@@ -27,23 +35,58 @@ describe('LogInForm', () => {
     expect(backBtn).toBeInTheDocument()
   })
 
-  // it('should render the logged in app page on log in submit', async () => {
-  //   submitUserLogIn.mockResolvedValueOnce({
-  //     user: {
-  //       id: 1, 
-  //       name: "Alan", 
-  //       email: "alan@turing.io"
-  //     }
-  //   })
-  //   const { getByText } = render(<LogInForm />)
-  //   const submitBtn = screen.getByRole('button', { name: /Log In/})
-  //   // fire event
-  //   fireEvent.click(screen.getByText('Log In'))
-  //   const pageTitle = await getByText('DOPE NOPE')
-  //   const welcomeMessage = await waitFor(() => getByText('Welcome Alan'));
-  //   // expect that it returns obj
-  //   expect(pageTitle).toBeInTheDocument()
-  //   expect(welcomeMessage).toBeInTheDocument()
-  //   // expect it renders homepage welcome message
-  // })
+  it.skip('should render the logged in app page on log in submit', async () => {
+    submitUserLogIn.mockResolvedValueOnce({
+      user: {
+        id: 1, 
+        name: "Alan", 
+        email: "alan@turing.io"
+      }
+    })
+
+    getUserMovieRatings.mockResolvedValueOnce(
+      {
+        ratings: [
+            {
+                id: 646,
+                user_id: 59,
+                movie_id: 451184,
+                rating: 2,
+                created_at: "2020-07-08T19:39:07.616Z",
+                updated_at: "2020-07-08T19:39:07.616Z"
+            },
+            {
+                id: 958,
+                user_id: 59,
+                movie_id: 508439,
+                rating: 2,
+                created_at: "2020-07-10T01:11:16.764Z",
+                updated_at: "2020-07-10T01:11:16.764Z"
+            }
+        ]
+      }
+    )
+
+    const { getByText, getByRole, getByPlaceholderText } = render(
+      <BrowserRouter>
+        <LogInForm />
+      </BrowserRouter>)
+    // const submitBtn = screen.getByRole('button', { name: /Log In/})
+
+    const emailInput = getByPlaceholderText('email')
+    const passwordInput = getByPlaceholderText('password')
+    const submitBtn = getByRole('button', {name: 'Log In'})
+
+    // fire event
+    const rightClick = { button: 2 }
+    fireEvent.change(emailInput, {target: {value: 'alan@turing.io'}})
+    fireEvent.change(passwordInput, {target: {value: '654321'}})
+    fireEvent.click(submitBtn)
+    const pageTitle = await waitFor(() => getByText('DOPE NOPE'))
+    const welcomeMessage = await waitFor(() => getByText('Welcome Alan'));
+    // expect that it returns obj
+    expect(pageTitle).toBeInTheDocument()
+    expect(welcomeMessage).toBeInTheDocument()
+    // expect it renders homepage welcome message
+  })
 })
