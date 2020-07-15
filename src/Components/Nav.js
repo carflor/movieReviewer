a99c9a5284c1a88393a54a0ea1dc9d0f9872c4a0
carflor <cfdeclet@gmail.com>
2020-07-15 16:22:52 -0600

    Linted nav file

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,54 +1,73 @@
 import React from 'react';
 import './Nav.scss';
-import LogInForm from './LogInForm'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import LogInForm from './LogInForm';
 
 const Nav = (props) => {
   if (props.data.form) {
     return (
-      <LogInForm form={props.data.form}/>
-    )
+      <LogInForm form={ props.data.form } />
+    );
   }
 
   if (props.data.isLoggedIn) {
     return (
       <nav className="nav">
-        <h1 className="nav-title">DOPE <span className="title-span">| |</span> NOPE</h1>
-          <p className="welcome-message">Welcome {props.data.user.name}</p>
+        <h1 className="nav-title">
+          DOPE
+          <span className="title-span">
+            | |
+          </span>
+          NOPE
+        </h1>
+        <p className="welcome-message">
+          Welcome
+          {props.data.user.name}
+        </p>
         <section className="nav-btn-box">
-            <Link to={props.data.showFavorites ? "/": "/favorites" }>
-              <button 
-                className={"dope-movie-btn"} 
-                onClick={props.data.showFavoritesBtn} 
-                style={{ textDecoration: 'none' }}>
-                {props.data.showFavorites ? 'ALL MOVIES' : 'DOPE MOVIES'}
-              </button>
-            </Link>
-          <button 
-            className={props.data.showFavorites ? "hidden" : "login-btn" }
-            onClick={props.data.logOutMethod}>LOG OUT
+          <Link to={props.data.showFavorites ? "/": "/favorites" }>
+            <button
+              type="button"
+              className={"dope-movie-btn"} 
+              onClick={props.data.showFavoritesBtn} 
+              style={{ textDecoration: 'none' }}
+            >
+              {props.data.showFavorites ? 'ALL MOVIES' : 'DOPE MOVIES'}
+            </button>
+          </Link>
+          <button
+            type="button"
+            className={props.data.showFavorites ? 'hidden' : 'login-btn'}
+            onClick={props.data.logOutMethod}
+          >
+            LOG OUT
           </button>
         </section>
       </nav>
-    )
+    );
   }
 
   return (
     <nav className="nav">
-      <h1 className="nav-title">DOPE <span className="title-span">| |</span>NOPE</h1>
-      <label></label>
+      <h1 className="nav-title">
+        DOPE
+        <span className="title-span">
+          | |
+        </span>
+        NOPE
+      </h1>
       <section className="nav-btn-box">
-        <Link to='/login' >
-          <button 
-            className="login-btn" 
+        <Link to="/login">
+          <button
+            type="button"
+            className="login-btn"
             onClick={props.data.logInMethod}>
-              LOG IN
+            LOG IN
           </button>
         </Link>
       </section>
     </nav>
-  )
-}
+  );
+};
 
-
-export default Nav
+export default Nav;
